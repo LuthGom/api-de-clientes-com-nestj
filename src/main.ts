cdaecd4cb786bec63aee934a8276438d2ddeb7f4
LuthGom <lucianomendes23@yahoo.com>
2023-01-19 14:27:29 -0300

    importando dotenv e mongoose para configs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import mongoose from 'mongoose';
+mongoose.set('strictQuery', true);
+mongoose.set('strictPopulate', false);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
