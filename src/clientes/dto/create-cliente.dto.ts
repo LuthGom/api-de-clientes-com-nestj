1a049c2a41bcca12805f42d21078fb4fe0cc3be0
LuthGom <lucianomendes23@yahoo.com>
2023-01-19 14:30:35 -0300

    add atributos do ClienteDto

--- src/clientes/dto/create-cliente.dto.ts
+++ src/clientes/dto/create-cliente.dto.ts
@@ -1,1 +1,11 @@
-export class CreateClienteDto {}
+import { Document } from 'mongoose';
+
+export class CreateClienteDto extends Document {
+  readonly name: String;
+  readonly username: Number;
+  readonly email: String;
+  readonly cpf: String;
+  readonly endereco: Object;
+  readonly password: String;
+  readonly createdAt: Date;
+}
