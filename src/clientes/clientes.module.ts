9aa6e2db8606bca930f0f8c8db496afe2c6e5b14
LuthGom <lucianomendes23@yahoo.com>
2023-01-19 14:28:44 -0300

    importando DatabaseModule e clientesProviders

--- src/clientes/clientes.module.ts
+++ src/clientes/clientes.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ClientesService } from './clientes.service';
 import { ClientesController } from './clientes.controller';
+import { DatabaseModule } from 'src/shared/database.module';
+import { clientesProviders } from './clientes.providers';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ClientesController],
-  providers: [ClientesService]
+  providers: [ClientesService, ...clientesProviders],
 })
 export class ClientesModule {}
