7daa40dbdead3e6bd594ddb8033d576156540238
LuthGom <lucianomendes23@yahoo.com>
2023-01-22 18:13:11 -0300

    adicionando AuthModule ao appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ClientesModule } from './clientes/clientes.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [ConfigModule.forRoot(), ClientesModule],
+  imports: [ConfigModule.forRoot(), ClientesModule, AuthModule],
   controllers: [],
   providers: [],
 })
