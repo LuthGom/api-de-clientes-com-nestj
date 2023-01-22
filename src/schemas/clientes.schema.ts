36b9d9928df13aae59faeba090b4baab3a4630ea
LuthGom <lucianomendes23@yahoo.com>
2023-01-22 18:05:17 -0300

    adicionando query de pesquisa por username à Schema

--- src/schemas/clientes.schema.ts
+++ src/schemas/clientes.schema.ts
@@ -1,11 +1,20 @@
 import * as mongoose from 'mongoose';
 
-export const ClienteSchema = new mongoose.Schema({
-  name: String,
-  username: String,
-  email: String,
-  cpf: String,
-  endereco: Object,
-  password: String,
-  createdAt: Date
-});
+export const ClienteSchema = new mongoose.Schema(
+  {
+    name: String,
+    username: String,
+    email: String,
+    cpf: String,
+    endereco: Object,
+    password: String,
+    createdAt: Date,
+  },
+  {
+    query: {
+      byUsername(username: string) {
+        return this.find({ username });
+      },
+    },
+  },
+);
