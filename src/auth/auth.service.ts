420aeeaa48d0e170e43137f6ba2200ece010cc41
LuthGom <lucianomendes23@yahoo.com>
2023-01-22 21:20:38 -0300

    Criando auth.service para condição de autenticação

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
-
+import { ClientesService } from 'src/clientes/clientes.service';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private clientesService: ClientesService) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.clientesService.findOne(username);
+
+    const senha = user[0].password;
+
+    if (user && senha === pass) {
+      const { password, ...result } = user[0];
+      return result;
+    }
+    return null;
+  }
+}
