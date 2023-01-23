7c0b72293a4e793c12a67b5cc739e355a85480d2
LuthGom <lucianomendes23@yahoo.com>
2023-01-22 21:18:42 -0300

    passando interface Cliente para model

--- src/clientes/clientes.providers.ts
+++ src/clientes/clientes.providers.ts
@@ -1,9 +1,12 @@
 import { Connection } from 'mongoose';
-import { ClienteSchema } from 'src/schemas/clientes.schema';
+import { Cliente } from 'src/interfaces/cliente.interface';
+import ClienteSchema from 'src/schemas/clientes.schema';
 export const clientesProviders = [
   {
     provide: 'CLIENTE_MODEL',
-    useFactory: (connection: Connection) => connection.model('Cliente', ClienteSchema),
+    useFactory: (connection: Connection) =>
+      connection.model<Cliente>('Cliente', ClienteSchema),
     inject: ['DATABASE_CONNECTION'],
   },
+  
 ];
